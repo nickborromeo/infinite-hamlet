26814f52ea679ee3efd18751493436fa3b50b89d
Nick Borromeo <ngborromeo@gmail.com>
2013-05-14 17:34:53 -0700

    Make public the public folder

--- merge.rb
+++ merge.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require './lib/report'
 
-set :public_folder, 'reports'
+set :public_folder, 'public'
 
 get "/" do
   erb :index 
@@ -16,7 +16,6 @@ post "/support" do
   report = Report.new
   s_report = report.generate_support_report(params['support-report'][:tempfile])
   
-  "Process Finished"
   send_file s_report, :filename => "merged_support_cases_#{Time.new}.xls"
 end
 
@@ -26,4 +25,10 @@ end
 
 post "/customer-service" do
 
+  report = Report.new
+  cs_report = report.generate_customer_service_report(params['customer-service-report'][:tempfile])
+
+  send_file cs_report, :filename => "merged_customer_service_cases_#{Time.new}.xls"
 end
+
+
